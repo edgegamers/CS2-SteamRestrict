29d015ac9df7a895ee744d4e481cb9b05aa71e66
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2024-04-06 15:23:59 +0200

    Release (1.2)

--- src/KitsuneSteamRestrict.cs
+++ src/KitsuneSteamRestrict.cs
@@ -12,7 +12,7 @@ using CounterStrikeSharp.API.Modules.Admin;
 
 namespace KitsuneSteamRestrict;
 
-public class KitsuneSteamRestrictConfig : BasePluginConfig
+public class PluginConfig : BasePluginConfig
 {
     [JsonPropertyName("SteamWebAPI")]
     public string SteamWebAPI { get; set; } = "";
@@ -44,51 +44,44 @@ public class KitsuneSteamRestrictConfig : BasePluginConfig
     [JsonPropertyName("BlockTradeBanned")]
     public bool BlockTradeBanned { get; set; } = false;
 
+    [JsonPropertyName("SteamGroupID")]
+    public string SteamGroupID { get; set; } = "";
+
     [JsonPropertyName("BlockGameBanned")]
     public bool BlockGameBanned { get; set; } = false;
 }
 
-[MinimumApiVersion(154)]
-public class SteamRestrictPlugin : BasePlugin, IPluginConfig<KitsuneSteamRestrictConfig>
+[MinimumApiVersion(198)]
+public class SteamRestrictPlugin : BasePlugin, IPluginConfig<PluginConfig>
 {
     public override string ModuleName => "Steam Restrict";
-    public override string ModuleVersion => "1.1.3";
+    public override string ModuleVersion => "1.2.0";
     public override string ModuleAuthor => "K4ryuu, Cruze @ KitsuneLab";
     public override string ModuleDescription => "Restrict certain players from connecting to your server.";
 
+    public readonly HttpClient Client = new HttpClient();
     private bool g_bSteamAPIActivated = false;
 
     private CounterStrikeSharp.API.Modules.Timers.Timer?[] g_hAuthorize = new CounterStrikeSharp.API.Modules.Timers.Timer?[65];
 
-    public class SteamUserInfo
-    {
-        public DateTime SteamAccountAge { get; set; }
-        public int SteamLevel { get; set; }
-        public int CS2Level { get; set; }
-        public int CS2Playtime { get; set; }
-        public bool IsPrivate { get; set; }
-        public bool HasPrime { get; set; }
-        public bool IsTradeBanned { get; set; }
-        public bool IsGameBanned { get; set; }
-    }
+    public PluginConfig Config { get; set; } = new();
 
-    public KitsuneSteamRestrictConfig Config { get; set; } = new();
-
-    public void OnConfigParsed(KitsuneSteamRestrictConfig config)
+    public void OnConfigParsed(PluginConfig config)
     {
         Config = config;
 
         if (string.IsNullOrEmpty(config.SteamWebAPI))
         {
-            Logger.LogError("[K4ryuuSteamRestrict] This plugin won't work because Web API is empty.");
+            Logger.LogError("This plugin won't work because Web API is empty.");
         }
     }
 
     public override void Load(bool hotReload)
     {
-        RegisterListener<Listeners.OnGameServerSteamAPIActivated>(OnGameServerSteamAPIActivated);
-        RegisterListener<Listeners.OnClientConnect>(OnClientConnect);
-        RegisterListener<Listeners.OnClientDisconnect>(OnClientDisconnect);
+        RegisterListener<Listeners.OnGameServerSteamAPIActivated>(() => { g_bSteamAPIActivated = true; });
+        RegisterListener<Listeners.OnClientConnect>((int slot, string name, string ipAddress) => { g_hAuthorize[slot]?.Kill(); });
+        RegisterListener<Listeners.OnClientDisconnect>((int slot) => { g_hAuthorize[slot]?.Kill(); });
+        RegisterEventHandler<EventPlayerConnectFull>(OnPlayerConnectFull, HookMode.Post);
 
         if (hotReload)
         {
@@ -101,31 +94,12 @@ public class SteamRestrictPlugin : BasePlugin, IPluginConfig<KitsuneSteamRestric
         }
     }
 
-    public override void Unload(bool hotReload)
-    {
-        base.Unload(hotReload);
-    }
-
-    private void OnClientConnect(int slot, string name, string ipAddress)
-    {
-        g_hAuthorize[slot]?.Kill();
-    }
-
-    private void OnClientDisconnect(int slot)
-    {
-        g_hAuthorize[slot]?.Kill();
-    }
-
-    private void OnGameServerSteamAPIActivated()
-    {
-        g_bSteamAPIActivated = true;
-    }
-
-    [GameEventHandler(HookMode.Post)]
     public HookResult OnPlayerConnectFull(EventPlayerConnectFull @event, GameEventInfo info)
     {
         CCSPlayerController player = @event.Userid;
-        if (player == null) return HookResult.Continue;
+
+        if (player == null)
+            return HookResult.Continue;
 
         OnPlayerConnectFull(player);
 
@@ -142,14 +116,11 @@ public class SteamRestrictPlugin : BasePlugin, IPluginConfig<KitsuneSteamRestric
 
         if (player.AuthorizedSteamID == null)
         {
-            Logger.LogInformation($"{player.PlayerName ?? "Unknown"} not validated. Waiting for him to get validated.");
-
             g_hAuthorize[player.Slot] = AddTimer(1.0f, () =>
             {
                 if (player.AuthorizedSteamID != null)
                 {
                     g_hAuthorize[player.Slot]?.Kill();
-                    Logger.LogInformation($"{player.PlayerName ?? "Unknown"} is now validated.");
                     OnPlayerConnectFull(player);
                     return;
                 }
@@ -161,49 +132,39 @@ public class SteamRestrictPlugin : BasePlugin, IPluginConfig<KitsuneSteamRestric
             return;
 
         nint handle = player.Handle;
-        ulong steamID = player.SteamID;
         ulong authorizedSteamID = player.AuthorizedSteamID.SteamId64;
 
-        _ = CheckUserViolations(handle, steamID, authorizedSteamID);
+        _ = CheckUserViolations(handle, authorizedSteamID);
     }
 
-    private async Task CheckUserViolations(nint handle, ulong steamID, ulong authorizedSteamID)
+    private async Task CheckUserViolations(nint handle, ulong authorizedSteamID)
     {
-        CSteamID cSteamID = new CSteamID(steamID);
+        SteamService steamService = new SteamService(this);
+        await steamService.FetchSteamUserInfoAsync(handle, authorizedSteamID);
 
-        SteamUserInfo userInfo = new SteamUserInfo
-        {
-            // Check for both CSGO's prime and CS2's prime.
-            HasPrime = SteamGameServer.UserHasLicenseForApp(cSteamID, (AppId_t)624820) == EUserHasLicenseForAppResult.k_EUserHasLicenseResultHasLicense
-                    || SteamGameServer.UserHasLicenseForApp(cSteamID, (AppId_t)54029) == EUserHasLicenseForAppResult.k_EUserHasLicenseResultHasLicense,
-            // Fetch CS2 Level
-            CS2Level = new CCSPlayerController_InventoryServices(handle).PersonaDataPublicLevel
-        };
-
-        SteamUserInfo updatedUserInfo = await FetchSteamUserInfo(userInfo, authorizedSteamID);
+        SteamUserInfo? userInfo = steamService.UserInfo;
 
         Server.NextWorldUpdate(() =>
         {
-            CCSPlayerController? player = Utilities.GetPlayerFromSteamId(steamID);
+            CCSPlayerController? player = Utilities.GetPlayerFromSteamId(authorizedSteamID);
 
-            if (player?.IsValid == true)
+            if (player?.IsValid == true && userInfo != null)
             {
-                Logger.LogInformation($"{player.PlayerName ?? "Unknown"} info:");
-                Logger.LogInformation($"CS2Playtime: {updatedUserInfo.CS2Playtime}");
-                Logger.LogInformation($"CS2Level: {updatedUserInfo.CS2Level}");
-                Logger.LogInformation($"SteamLevel: {updatedUserInfo.SteamLevel}");
-                if ((DateTime.Now - updatedUserInfo.SteamAccountAge).TotalSeconds > 30)
+                Logger.LogInformation($"{player.PlayerName} info:");
+                Logger.LogInformation($"CS2Playtime: {userInfo.CS2Playtime}");
+                Logger.LogInformation($"CS2Level: {userInfo.CS2Level}");
+                Logger.LogInformation($"SteamLevel: {userInfo.SteamLevel}");
+                if ((DateTime.Now - userInfo.SteamAccountAge).TotalSeconds > 30)
                     Logger.LogInformation($"Steam Account Creation Date: {userInfo.SteamAccountAge:dd-MM-yyyy}");
                 else
                     Logger.LogInformation($"Steam Account Creation Date: N/A");
-                Logger.LogInformation($"HasPrime: {updatedUserInfo.HasPrime}");
-                Logger.LogInformation($"HasPrivateProfile: {updatedUserInfo.IsPrivate}");
-                Logger.LogInformation($"IsTradeBanned: {updatedUserInfo.IsTradeBanned}");
-                Logger.LogInformation($"IsGameBanned: {updatedUserInfo.IsGameBanned}");
+                Logger.LogInformation($"HasPrime: {userInfo.HasPrime}");
+                Logger.LogInformation($"HasPrivateProfile: {userInfo.IsPrivate}");
+                Logger.LogInformation($"IsTradeBanned: {userInfo.IsTradeBanned}");
+                Logger.LogInformation($"IsGameBanned: {userInfo.IsGameBanned}");
+                Logger.LogInformation($"IsInSteamGroup: {userInfo.IsInSteamGroup}");
 
-                var x = player.Handle;
-
-                if (IsRestrictionViolated(player, updatedUserInfo))
+                if (IsRestrictionViolated(player, userInfo))
                 {
                     Server.ExecuteCommand($"kickid {player.UserId} \"You have been kicked for not meeting the minimum requirements.\"");
                 }
@@ -211,246 +172,27 @@ public class SteamRestrictPlugin : BasePlugin, IPluginConfig<KitsuneSteamRestric
         });
     }
 
-    private async Task<SteamUserInfo> FetchSteamUserInfo(SteamUserInfo userInfo, ulong authorizedSteamID)
-    {
-        using (HttpClient httpClient = new HttpClient())
-        {
-            string steamId = authorizedSteamID.ToString();
-            string steamWebAPIKey = Config.SteamWebAPI;
-
-            // Fetch CS2 Playtime
-            string gamesUrl = $"https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={steamWebAPIKey}&steamid={steamId}&format=json";
-            HttpResponseMessage gamesResponse = await httpClient.GetAsync(gamesUrl);
-
-            if (gamesResponse.IsSuccessStatusCode)
-            {
-                string gamesJson = await gamesResponse.Content.ReadAsStringAsync();
-                userInfo.CS2Playtime = ParseCS2Playtime(gamesJson) / 60;
-            }
-            else
-            {
-                userInfo.CS2Playtime = 0;
-            }
-
-            // Fetch Steam Level
-            string levelUrl = $"http://api.steampowered.com/IPlayerService/GetSteamLevel/v1/?key={steamWebAPIKey}&steamid={steamId}";
-            HttpResponseMessage levelResponse = await httpClient.GetAsync(levelUrl);
-
-            if (levelResponse.IsSuccessStatusCode)
-            {
-                string levelJson = await levelResponse.Content.ReadAsStringAsync();
-                userInfo.SteamLevel = ParseSteamLevel(levelJson);
-            }
-            else
-            {
-                userInfo.SteamLevel = 0;
-            }
-
-            // Fetch Profile Privacy
-            string userUrl = $"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={steamWebAPIKey}&steamids={steamId}";
-            HttpResponseMessage userResponse = await httpClient.GetAsync(userUrl);
-
-            if (userResponse.IsSuccessStatusCode)
-            {
-                string userJson = await userResponse.Content.ReadAsStringAsync();
-                ParseSteamUserInfo(userJson, userInfo);
-            }
-            else
-            {
-                userInfo.IsPrivate = false;
-            }
-
-            // Fetch TradeBan Status
-            string tradeBanUrl = $"https://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key={steamWebAPIKey}&steamids={steamId}";
-            HttpResponseMessage tradeBanResponse = await httpClient.GetAsync(tradeBanUrl);
-
-            if (tradeBanResponse.IsSuccessStatusCode)
-            {
-                string tradeBanJson = await tradeBanResponse.Content.ReadAsStringAsync();
-                ParseTradeBanStatus(tradeBanJson, userInfo);
-            }
-            else
-            {
-                userInfo.IsTradeBanned = false;
-            }
-
-            // Fetch GameBan Status
-            string gameBanUrl = $"https://api.steampowered.com/ISteamUser/GetUserGameBan/v1/?key={steamWebAPIKey}&steamids={steamId}";
-            HttpResponseMessage gameBanResponse = await httpClient.GetAsync(gameBanUrl);
-
-            if (gameBanResponse.IsSuccessStatusCode)
-            {
-                string gameBanJson = await gameBanResponse.Content.ReadAsStringAsync();
-                ParseGameBanStatus(gameBanJson, userInfo);
-            }
-            else
-            {
-                userInfo.IsGameBanned = false;
-            }
-        }
-
-        return userInfo;
-    }
-
     private bool IsRestrictionViolated(CCSPlayerController player, SteamUserInfo userInfo)
     {
-        bool isViolated = false;
+        if (AdminManager.PlayerHasPermissions(player, "@css/bypasspremiumcheck"))
+            return false;
 
-        if (userInfo.HasPrime)
+        bool isPrime = userInfo.HasPrime;
+        var configChecks = new[]
         {
-            if ((Config.MinimumHourPrime != -1 && userInfo.CS2Playtime < Config.MinimumHourPrime)
-                || (Config.MinimumLevelPrime != -1 && userInfo.SteamLevel < Config.MinimumLevelPrime)
-                || (Config.MinimumCS2LevelPrime != -1 && userInfo.CS2Level < Config.MinimumCS2LevelPrime))
-            {
-                isViolated = true;
-            }
-        }
-        else
-        {
-            if ((Config.MinimumHourNonPrime != -1 && userInfo.CS2Playtime < Config.MinimumHourNonPrime)
-                || (Config.MinimumLevelNonPrime != -1 && userInfo.SteamLevel < Config.MinimumLevelNonPrime)
-                || (Config.MinimumCS2LevelNonPrime != -1 && userInfo.CS2Level < Config.MinimumCS2LevelNonPrime))
-            {
-                isViolated = true;
-            }
-        }
+            (isPrime, Config.MinimumHourPrime, userInfo.CS2Playtime),
+            (isPrime, Config.MinimumLevelPrime, userInfo.SteamLevel),
+            (isPrime, Config.MinimumCS2LevelPrime, userInfo.CS2Level),
+            (!isPrime, Config.MinimumHourNonPrime, userInfo.CS2Playtime),
+            (!isPrime, Config.MinimumLevelNonPrime, userInfo.SteamLevel),
+            (!isPrime, Config.MinimumCS2LevelNonPrime, userInfo.CS2Level),
+            (true, Config.MinimumSteamAccountAgeInDays, (DateTime.Now - userInfo.SteamAccountAge).TotalDays),
+            (Config.BlockPrivateProfile, 1, userInfo.IsPrivate ? 0 : 1),
+            (Config.BlockTradeBanned, 1, userInfo.IsTradeBanned ? 0 : 1),
+            (Config.BlockGameBanned, 1, userInfo.IsGameBanned ? 0 : 1),
+            (!string.IsNullOrEmpty(Config.SteamGroupID), 1, userInfo.IsInSteamGroup ? 0 : 1)
+        };
 
-        if (Config.MinimumSteamAccountAgeInDays != -1)
-        {
-            if ((DateTime.Now - userInfo.SteamAccountAge).TotalDays < Config.MinimumSteamAccountAgeInDays)
-            {
-                isViolated = true;
-            }
-        }
-
-        if (Config.BlockPrivateProfile && userInfo.IsPrivate)
-        {
-            isViolated = true;
-        }
-
-        if (Config.BlockTradeBanned && userInfo.IsTradeBanned)
-        {
-            isViolated = true;
-        }
-
-        if (Config.BlockGameBanned && userInfo.IsGameBanned)
-        {
-            isViolated = true;
-        }
-
-        if (AdminManager.PlayerHasPermissions(player, "@css/bypasspremiumcheck") && isViolated)
-        {
-            isViolated = false;
-        }
-
-        return isViolated;
-    }
-
-    private int ParseCS2Playtime(string json)
-    {
-        int csPlaytime = 0;
-
-        try
-        {
-            JObject data = JObject.Parse(json);
-            JArray games = (data["response"]!["games"] as JArray)!;
-
-            if (games != null)
-            {
-                foreach (var game in games)
-                {
-                    int appId = (int)game["appid"]!;
-                    if (appId == 730)
-                    {
-                        csPlaytime = (int)game["playtime_forever"]!;
-                    }
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            Logger.LogError($"[K4ryuuSteamRestrict] Error parsing CS2 playtime: {ex.Message}");
-        }
-
-        return csPlaytime;
-    }
-
-    private int ParseSteamLevel(string json)
-    {
-        int steamLevel = 0;
-
-        try
-        {
-            JObject data = JObject.Parse(json);
-            steamLevel = (int)data["response"]!["player_level"]!;
-        }
-        catch (Exception ex)
-        {
-            Logger.LogError($"[K4ryuuSteamRestrict] Error parsing Steam level: {ex.Message}");
-        }
-
-        return steamLevel;
-    }
-
-    private void ParseSteamUserInfo(string json, SteamUserInfo userInfo)
-    {
-        try
-        {
-            JObject data = JObject.Parse(json);
-            JArray players = (data["response"]!["players"] as JArray)!;
-
-            if (players != null && players.Count > 0)
-            {
-                var player = players[0];
-
-                userInfo.IsPrivate = (int)player["communityvisibilitystate"]! != 3;
-                if (player["timecreated"] != null && (int)player["timecreated"]! > 0)
-                    userInfo.SteamAccountAge = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds((int)player["timecreated"]!);
-                else
-                    userInfo.SteamAccountAge = DateTime.Now;
-            }
-        }
-        catch (Exception ex)
-        {
-            Logger.LogError($"[K4ryuuSteamRestrict] Error parsing Steam user info: {ex.Message}");
-        }
-    }
-
-    private void ParseTradeBanStatus(string json, SteamUserInfo userInfo)
-    {
-        try
-        {
-            JObject data = JObject.Parse(json);
-            JArray playerBans = (data["players"] as JArray)!;
-
-            if (playerBans != null && playerBans.Count > 0)
-            {
-                var playerBan = playerBans[0];
-                userInfo.IsTradeBanned = (bool)playerBan["CommunityBanned"]!;
-            }
-        }
-        catch (Exception ex)
-        {
-            Logger.LogError($"[K4ryuuSteamRestrict] Error parsing trade ban status: {ex.Message}");
-        }
-    }
-
-    private void ParseGameBanStatus(string json, SteamUserInfo userInfo)
-    {
-        try
-        {
-            JObject data = JObject.Parse(json);
-            JArray userGameBans = (data["players"] as JArray)!;
-
-            if (userGameBans != null && userGameBans.Count > 0)
-            {
-                var userGameBan = userGameBans[0];
-                userInfo.IsGameBanned = (bool)userGameBan["IsGameBanned"]!;
-            }
-        }
-        catch (Exception ex)
-        {
-            Logger.LogError($"[K4ryuuSteamRestrict] Error parsing game ban status: {ex.Message}");
-        }
+        return configChecks.Any(check => check.Item1 && check.Item2 != -1 && check.Item3 < check.Item2);
     }
 }
---
+++ src/Models/SteamService.cs
@@ -0,0 +1,174 @@
+
+using CounterStrikeSharp.API.Core;
+using KitsuneSteamRestrict;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Steamworks;
+
+public class SteamUserInfo
+{
+	public DateTime SteamAccountAge { get; set; }
+	public int SteamLevel { get; set; }
+	public int CS2Level { get; set; }
+	public int CS2Playtime { get; set; }
+	public bool IsPrivate { get; set; }
+	public bool HasPrime { get; set; }
+	public bool IsTradeBanned { get; set; }
+	public bool IsGameBanned { get; set; }
+	public bool IsInSteamGroup { get; set; }
+}
+
+public class SteamService
+{
+	private readonly HttpClient _httpClient;
+	private readonly string _steamWebAPIKey;
+	private readonly PluginConfig _config;
+	private readonly ILogger _logger;
+	public SteamUserInfo? UserInfo = null;
+
+	public SteamService(SteamRestrictPlugin plugin)
+	{
+		_httpClient = plugin.Client;
+		_config = plugin.Config;
+		_logger = plugin.Logger;
+		_steamWebAPIKey = _config.SteamWebAPI;
+	}
+
+	public async Task FetchSteamUserInfoAsync(nint handle, ulong authorizedSteamID)
+	{
+		CSteamID cSteamID = new CSteamID(authorizedSteamID);
+
+		UserInfo = new SteamUserInfo
+		{
+			HasPrime = SteamGameServer.UserHasLicenseForApp(cSteamID, (AppId_t)624820) == EUserHasLicenseForAppResult.k_EUserHasLicenseResultHasLicense
+					|| SteamGameServer.UserHasLicenseForApp(cSteamID, (AppId_t)54029) == EUserHasLicenseForAppResult.k_EUserHasLicenseResultHasLicense,
+			CS2Level = new CCSPlayerController_InventoryServices(handle).PersonaDataPublicLevel
+		};
+
+		string steamId = authorizedSteamID.ToString();
+
+		UserInfo.CS2Playtime = await FetchCS2PlaytimeAsync(steamId) / 60;
+		UserInfo.SteamLevel = await FetchSteamLevelAsync(steamId);
+		await FetchProfilePrivacyAsync(steamId, UserInfo);
+		await FetchTradeBanStatusAsync(steamId, UserInfo);
+		await FetchGameBanStatusAsync(steamId, UserInfo);
+		await FetchSteamGroupMembershipAsync(steamId, UserInfo);
+	}
+
+	private async Task<int> FetchCS2PlaytimeAsync(string steamId)
+	{
+		var url = $"https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={_steamWebAPIKey}&steamid={steamId}&format=json";
+		var json = await GetApiResponseAsync(url);
+		return json != null ? ParseCS2Playtime(json) : 0;
+	}
+
+	private async Task<int> FetchSteamLevelAsync(string steamId)
+	{
+		var url = $"http://api.steampowered.com/IPlayerService/GetSteamLevel/v1/?key={_steamWebAPIKey}&steamid={steamId}";
+		var json = await GetApiResponseAsync(url);
+		return json != null ? ParseSteamLevel(json) : 0;
+	}
+
+	private async Task FetchProfilePrivacyAsync(string steamId, SteamUserInfo userInfo)
+	{
+		var url = $"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={_steamWebAPIKey}&steamids={steamId}";
+		var json = await GetApiResponseAsync(url);
+		if (json != null) ParseSteamUserInfo(json, userInfo);
+	}
+
+	private async Task FetchTradeBanStatusAsync(string steamId, SteamUserInfo userInfo)
+	{
+		var url = $"https://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key={_steamWebAPIKey}&steamids={steamId}";
+		var json = await GetApiResponseAsync(url);
+		if (json != null) ParseTradeBanStatus(json, userInfo);
+	}
+
+	private async Task FetchGameBanStatusAsync(string steamId, SteamUserInfo userInfo)
+	{
+		var url = $"https://api.steampowered.com/ISteamUser/GetUserGameBan/v1/?key={_steamWebAPIKey}&steamids={steamId}";
+		var json = await GetApiResponseAsync(url);
+		if (json != null) ParseGameBanStatus(json, userInfo);
+	}
+
+	private async Task FetchSteamGroupMembershipAsync(string steamId, SteamUserInfo userInfo)
+	{
+		if (!string.IsNullOrEmpty(_config.SteamGroupID))
+		{
+			var url = $"http://api.steampowered.com/ISteamUser/GetUserGroupList/v1/?key={_steamWebAPIKey}&steamid={steamId}";
+			var json = await GetApiResponseAsync(url);
+
+			userInfo.IsInSteamGroup = false;
+			if (json != null)
+			{
+				JObject jsonObj = JObject.Parse(json);
+				JToken? groups = jsonObj["response"]?["groups"];
+				if (groups != null)
+				{
+					userInfo.IsInSteamGroup = groups.Any(group => (group["gid"]?.ToString() ?? "") == _config.SteamGroupID);
+				}
+			}
+		}
+		else
+		{
+			userInfo.IsInSteamGroup = true;
+		}
+	}
+
+	private async Task<string?> GetApiResponseAsync(string url)
+	{
+		try
+		{
+			var response = await _httpClient.GetAsync(url);
+			if (response.IsSuccessStatusCode)
+			{
+				return await response.Content.ReadAsStringAsync();
+			}
+		}
+		catch (Exception e)
+		{
+			_logger.LogError($"An error occurred while fetching API response: {e.Message}");
+		}
+		return null;
+	}
+
+	private int ParseCS2Playtime(string json)
+	{
+		JObject data = JObject.Parse(json);
+		JToken? game = data["response"]?["games"]?.FirstOrDefault(x => x["appid"]?.Value<int>() == 730);
+		return game?["playtime_forever"]?.Value<int>() ?? 0;
+	}
+
+	private int ParseSteamLevel(string json)
+	{
+		JObject data = JObject.Parse(json);
+		return (int)(data["response"]?["player_level"] ?? 0);
+	}
+
+	private void ParseSteamUserInfo(string json, SteamUserInfo userInfo)
+	{
+		JObject data = JObject.Parse(json);
+		JToken? player = data["response"]?["players"]?.FirstOrDefault();
+		if (player != null)
+		{
+			userInfo.IsPrivate = player["communityvisibilitystate"]?.ToObject<int?>() != 3;
+			int? timeCreated = player["timecreated"]?.ToObject<int?>();
+			userInfo.SteamAccountAge = timeCreated.HasValue
+				? new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(timeCreated.Value)
+				: DateTime.Now;
+		}
+	}
+
+	private void ParseTradeBanStatus(string json, SteamUserInfo userInfo)
+	{
+		JObject data = JObject.Parse(json);
+		JToken? playerBan = data["players"]?.FirstOrDefault();
+		userInfo.IsTradeBanned = playerBan != null && (bool)(playerBan["CommunityBanned"] ?? false);
+	}
+
+	private void ParseGameBanStatus(string json, SteamUserInfo userInfo)
+	{
+		JObject data = JObject.Parse(json);
+		JToken? userGameBan = data["players"]?.FirstOrDefault();
+		userInfo.IsGameBanned = userGameBan != null && (bool)(userGameBan["IsGameBanned"] ?? false);
+	}
+}
